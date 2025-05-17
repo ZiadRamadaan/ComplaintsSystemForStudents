475d603b587a27c1d50fac9687504c080b8dc28a
esraa elmaghraby <esraamaghrabi14@gmail.com>
2025-05-17 14:03:12 +0300

    Update notifications.py

--- notifications.py
+++ notifications.py
@@ -1,7 +1,11 @@
 import streamlit as st
+import time
 
 def display_notifications():
-    if "notifications" in st.session_state and st.session_state.notifications:
-        for message in st.session_state.notifications:
-            st.success(message)
-        st.session_state.notifications.clear()
+    if "notifications" in st.session_state and len(st.session_state.notifications) > 0:
+        for notification in st.session_state.notifications:
+            notification_expander = st.expander(notification, expanded=True)
+            with notification_expander:
+                st.write(notification) 
+            time.sleep(2)
+            st.session_state.notifications.remove(notification)
