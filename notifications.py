c4f4fccabdd88bcdfad3d7137b80cc1c1b69adcb
esraa elmaghraby <esraamaghrabi14@gmail.com>
2025-05-17 14:06:55 +0300

    Update notifications.py

--- notifications.py
+++ notifications.py
@@ -4,8 +4,6 @@ import time
 def display_notifications():
     if "notifications" in st.session_state and len(st.session_state.notifications) > 0:
         for notification in st.session_state.notifications:
-            notification_expander = st.expander(notification, expanded=True)
-            with notification_expander:
-                st.success(notification) 
+            st.success(notification) 
             time.sleep(2)
             st.session_state.notifications.remove(notification)
